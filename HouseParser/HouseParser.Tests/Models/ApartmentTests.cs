d753db2c09b968ac9d3f4a0327203f851d03e05c
dustwind <dustwind23@gmail.com>
2020-04-07 18:13:26 +0700

    Add solution. Add Apartment model.

---
+++ HouseParser/HouseParser.Tests/Models/ApartmentTests.cs
@@ -0,0 +1,39 @@
+﻿using HouseParser.Models;
+using System;
+using Xunit;
+
+namespace HouseParser.Tests.Models
+{
+    public class ApartmentTests
+    {
+        [Fact]
+        public void ApartmantCreate()
+        {
+            Assert.NotNull(new Apartment("", 1, 1, 1, 1));
+        }
+
+        [Fact]
+        public void ErrorIfAreaBelowZero()
+        {
+            var area = -1d;
+
+            Assert.Throws<ArgumentException>(() => new Apartment("", area, 1, 1, 1));
+        }
+
+        [Fact]
+        public void ErrorIfRoomBelowZero()
+        {
+            var rooms = -1;
+
+            Assert.Throws<ArgumentException>(() => new Apartment("", 1, rooms, 1, 1));
+        }
+
+        [Fact]
+        public void ErrorIfPriceBelowZero()
+        {
+            var price = -1d;
+
+            Assert.Throws<ArgumentException>(() => new Apartment("", 1, 1, 1, price));
+        }
+    }
+}
---
+++ HouseParser/HouseParser/Models/Apartment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HouseParser.Models
+{
+    public class Apartment
+    {
+        public string City { get; private set; }
+
+        public double Area { get; private set; }
+
+        public int Rooms { get; private set; }
+
+        public int Floor { get; private set; }
+
+        public double Price { get; private set; }
+
+        public Apartment(string city, double area, int rooms, int floor, double price)
+        {
+            if (area < 0)
+            {
+                throw new ArgumentException("Area can not be negative.");
+            }
+
+            if (rooms < 0)
+            {
+                throw new ArgumentException("Rooms can not be negative.");
+            }
+
+            if (price < 0)
+            {
+                throw new ArgumentException("Price can not be negative.");
+            }
+
+            City = city;
+            Floor = floor;
+            Area = area;
+            Rooms = rooms;
+            Price = price;
+        }
+    }
+}
